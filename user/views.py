59304c78fb369b52f4991378f179fc39632f3daf
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-12-24 15:44:40 +0200

    "fix JWT authentication"

--- user/views.py
+++ user/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from user.serializers import UserSerializer
 
 
@@ -13,6 +12,7 @@ class CreateUserView(generics.CreateAPIView):
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
 
     def get_object(self):
         return self.request.user
